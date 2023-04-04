b682919ac4c20f9ff6c68be1cdb34ba73a66d0bc
kiv1n <aloha@kiv1n.ru>
2023-04-04 12:53:11 +0300

    Update errors library abi

--- packages/nextjs/contracts/abi/errorsLibrary.ts
+++ packages/nextjs/contracts/abi/errorsLibrary.ts
@@ -1,6 +1,41 @@
 export const errorsLibraryAbi = [
   {
     inputs: [],
+    name: "ContributionClosed",
+    type: "error",
+  },
+  {
+    inputs: [],
+    name: "MessageValueMismatch",
+    type: "error",
+  },
+  {
+    inputs: [],
+    name: "NotAuthor",
+    type: "error",
+  },
+  {
+    inputs: [],
+    name: "NotPotentialContributor",
+    type: "error",
+  },
+  {
+    inputs: [],
+    name: "RewardInvalid",
+    type: "error",
+  },
+  {
+    inputs: [],
+    name: "SendingRewardFailed",
+    type: "error",
+  },
+  {
+    inputs: [],
+    name: "TokenDoesNotExist",
+    type: "error",
+  },
+  {
+    inputs: [],
     name: "TokenNotTransferable",
     type: "error",
   },
